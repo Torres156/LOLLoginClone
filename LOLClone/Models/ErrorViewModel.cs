3b6e0d0d031c213a43255d3b21e72318f59db46f
Gustavo Santos <gts_br@yahoo.com.br>
2022-01-19 11:49:21 -0300

    Adicionar arquivos de projeto.

---
+++ LOLClone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LOLClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
